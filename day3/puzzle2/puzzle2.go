34f1ca19fe8b90df9d556986fd166d0472c74d5b
Mario de la Ossa <mariodelaossa@gmail.com>
2017-12-11 14:55:38 -0600

    Day 3 puzzle 2 done

--- day3/puzzle2/puzzle2.go
+++ day3/puzzle2/puzzle2.go
@@ -1,9 +1,55 @@
 package puzzle2
 
-// Solver knows how to solze puzze 1 from Day 3
+// Solver knows how to solze puzze 2 from Day 3
 type Solver struct{}
 
-// Solve actually solves puzze 1 from Day 3
+// More convenient way to populate the spiral than using nested maps
+type k struct {
+	x, y int
+}
+
+// Solve actually solves puzze 2 from Day 3
 func (s *Solver) Solve(input int) int {
-	return 0
+	return buildSpiralUntil(input)
+}
+
+func buildSpiralUntil(n int) int {
+	neighbors := [8]k{k{1, 0}, k{1, 1}, k{0, 1}, k{-1, 1}, k{-1, 0}, k{-1, -1}, k{0, -1}, k{1, -1}}
+	directions := [4]k{k{1, 0}, k{0, 1}, k{-1, 0}, k{0, -1}}
+
+	spiral := make(map[k]int)
+	spiral[k{0, 0}] = 1
+	x, y := 0, 0
+	steps := 1
+	secondDirection := false
+	nstep := 0
+	direction := 0
+
+	for {
+		v := directions[direction]
+		x, y = x+v.x, y+v.y
+		total := 0
+		for _, neighbor := range neighbors {
+			coord := k{neighbor.x + x, neighbor.y + y}
+			if val, ok := spiral[coord]; ok {
+				total += val
+			}
+		}
+
+		if total > n {
+			return total
+		}
+		spiral[k{x, y}] = total
+		nstep++
+		if nstep == steps {
+			nstep = 0
+			direction = (direction + 1) % 4
+			if secondDirection {
+				secondDirection = false
+				steps++
+			} else {
+				secondDirection = true
+			}
+		}
+	}
 }
--- day3/puzzle2/puzzle2_test.go
+++ day3/puzzle2/puzzle2_test.go
@@ -1,1 +1,22 @@
 package puzzle2
+
+import "testing"
+
+func TestSolve(t *testing.T) {
+	cases := []struct {
+		in, want int
+	}{
+		{1, 2},
+		{12, 23},
+		{23, 25},
+		{747, 806},
+	}
+
+	for _, c := range cases {
+		solver := Solver{}
+		got := solver.Solve(c.in)
+		if got != c.want {
+			t.Errorf("Solve(%v) == %v, wanted: %v\n", c.in, got, c.want)
+		}
+	}
+}
