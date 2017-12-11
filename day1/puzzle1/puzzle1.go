ccf0c2267a8958c4e6605db7123cd927794a6c30
Mario de la Ossa <mariodelaossa@gmail.com>
2017-12-11 14:58:55 -0600

    Day 1 - modulo is a thing, use it

--- day1/puzzle1/puzzle1.go
+++ day1/puzzle1/puzzle1.go
@@ -4,26 +4,23 @@ type Puzzle1Solver struct{}
 
 func (s Puzzle1Solver) Solve(values []int) (sum int) {
 
-  for _, v := range findAdjacent(values) {
-    sum += v
-  }
+	for _, v := range findAdjacent(values) {
+		sum += v
+	}
 
-  return
+	return
 }
 
-func findAdjacent(values [] int) (toSum []int) {
-  toSum = make([]int, 0, len(values)/2)
+func findAdjacent(values []int) (toSum []int) {
+	toSum = make([]int, 0, len(values)/2)
 
-  for i, val := range values {
-    nextIndex := i + 1
-    if nextIndex >= len(values) {
-      nextIndex = 0
-    }
+	for i, val := range values {
+		nextIndex := (i + 1) % len(values)
 
-    if val == values[nextIndex] {
-      toSum = append(toSum, val)
-    }
-  }
+		if val == values[nextIndex] {
+			toSum = append(toSum, val)
+		}
+	}
 
-  return
+	return
 }
--- day1/puzzle2/puzzle2.go
+++ day1/puzzle2/puzzle2.go
@@ -4,27 +4,24 @@ type Puzzle2Solver struct{}
 
 func (s Puzzle2Solver) Solve(values []int) (sum int) {
 
-  for _, v := range evaluatePair(values) {
-    sum += v
-  }
+	for _, v := range evaluatePair(values) {
+		sum += v
+	}
 
-  return
+	return
 }
 
 func evaluatePair(values []int) (toSum []int) {
-  toSum = make([]int, 0, len(values)/2)
-  length := len(values)
+	toSum = make([]int, 0, len(values)/2)
+	length := len(values)
 
-  for i, val := range values {
-    nextIndex := i + length/2
-    if nextIndex >= length {
-      nextIndex -= length
-    }
+	for i, val := range values {
+		nextIndex := (i + length/2) % length
 
-    if val == values[nextIndex] {
-      toSum = append(toSum, val)
-    }
-  }
+		if val == values[nextIndex] {
+			toSum = append(toSum, val)
+		}
+	}
 
-  return
+	return
 }
