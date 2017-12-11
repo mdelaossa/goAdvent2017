2f5eb0265398cb1f33d84dcd164a0785df5aafa7
Mario de la Ossa <mariodelaossa@gmail.com>
2017-12-11 14:16:53 -0600

    Day 3 puzzle 1 - switched to using goroutines and channels
    Because when you have a shiny new hammer everything tends to look like a
    nail.

--- day3/puzzle1/puzzle1.go
+++ day3/puzzle1/puzzle1.go
@@ -1,6 +1,9 @@
 package puzzle1
 
-import "math"
+import (
+	"math"
+	"sync"
+)
 
 // Solver knows how to solze puzze 1 from Day 3
 type Solver struct{}
@@ -32,10 +35,25 @@ func closestMidpoint(input int, length int) int {
 	offset := int((length - 1) / 2.0)
 	closestMidpoint := 0
 
-	for i := 0; i < 4; i++ {
-		midpoint := highestValue - (offset * (2*i + 1))
-		length--
+	wg := sync.WaitGroup{}
+	ch := make(chan int)
 
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		// When you have a shiny new hammer, everything looks like a nail
+		go func(wg *sync.WaitGroup, ch chan int, iteration int) {
+			defer wg.Done()
+			midpoint := highestValue - (offset * (2*iteration + 1))
+			ch <- midpoint
+		}(&wg, ch, i)
+	}
+
+	go func(wg *sync.WaitGroup, ch chan int) {
+		wg.Wait()
+		close(ch)
+	}(&wg, ch)
+
+	for midpoint := range ch {
 		dst := input - midpoint
 		if dst < 0 {
 			dst = -dst
